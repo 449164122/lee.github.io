c2d0c30d3bd7f77ed970973f0967151f0ae4ce44
litao4491 <litao@ztccloud.com.cn>
2022-08-30 10:59:27 +0800

    --story=1103289 --user=李韬 3.7-UI平台组体验优化（父需求） https://www.tapd.cn/67410840/s/2802356

--- packages/directives/detail.js
+++ packages/directives/detail.js
@@ -2,15 +2,15 @@
  * @Author: 李韬
  * @Date: 2022-08-11 09:06:50
  * @LastEditors: 李韬
- * @LastEditTime: 2022-08-12 10:54:24
+ * @LastEditTime: 2022-08-30 10:50:19
  */
 export default {
-  inserted: function(el) {
+  inserted: function(el, bind = {}) {
     el.addEventListener('dblclick', ()=>{
       const detailDom = document.getElementById('zfs-check-detail');
       if (detailDom) {
         detailDom.style.display = 'flex';
-        detailDom.getElementsByTagName('div')[0].innerText = el.innerText;
+        detailDom.getElementsByTagName('div')[0].innerText = bind.value || el.innerText;
         setTimeout(()=>{
           detailDom.style.opacity = 1;
         },0)
@@ -26,7 +26,7 @@ export default {
         newEl.style.cssText = "position: fixed; top: 0; left: 0; bottom: 0; right: 0; z-index: 1000; background: #fff; display: flex; align-items: center; justify-content: center; padding: 0 50px; box-sizing: border-box;overflow-y: auto; font-size: 20px;opacity: 0;transition: opacity 0.4s;" 
         const newElChild= document.createElement('div');
         newElChild.style.cssText = "margin: auto";
-        newElChild.innerText = el.innerText;
+        newElChild.innerText = bind.value || el.innerText;
         newEl.appendChild(newElChild)
         document.body.appendChild(newEl);
         setTimeout(()=>{
