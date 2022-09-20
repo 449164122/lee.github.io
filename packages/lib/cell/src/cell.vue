fe17ae004416150118448c9a093c1b72cb8f8fd2
litao4491 <litao@ztccloud.com.cn>
2022-09-20 17:10:12 +0800

    去掉title slot

--- packages/lib/cell/src/cell.vue
+++ packages/lib/cell/src/cell.vue
@@ -2,15 +2,13 @@
  * @Author: 李韬
  * @Date: 2022-08-23 13:47:42
  * @LastEditors: 李韬
- * @LastEditTime: 2022-08-29 17:22:45
+ * @LastEditTime: 2022-09-20 17:08:23
 -->
 <template>
   <div :class="['zfs-cell', {'dark': touched && isLink}]" @touchstart="handlertouch" @touchend="touchend" @touchcancel="touchend">
     <div class="zfs-cell__title" v-if="title">
-      <slot name="title">
         <span v-detail>{{ title }}</span>
         <div class="zfs-cell__label" v-if="label">{{ label }}</div>
-      </slot>
     </div> 
     <div :class="['zfs-cell__value', { 'zfs-cell__value--alone': value && !title }]">
         <span v-detail>{{ value }}</span>
