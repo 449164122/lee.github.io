adaf4d268c96fc2c59e85b8a6465961f7e9494c8
litao <litao@ztccloud.com.cn>
2022-11-18 11:42:55 +0800

    数字字母换行

--- packages/directives/detail.js
+++ packages/directives/detail.js
@@ -2,7 +2,7 @@
  * @Author: 李韬
  * @Date: 2022-08-11 09:06:50
  * @LastEditors: 李韬
- * @LastEditTime: 2022-08-30 10:50:19
+ * @LastEditTime: 2022-11-18 11:42:11
  */
 export default {
   inserted: function(el, bind = {}) {
@@ -25,7 +25,7 @@ export default {
         }
         newEl.style.cssText = "position: fixed; top: 0; left: 0; bottom: 0; right: 0; z-index: 1000; background: #fff; display: flex; align-items: center; justify-content: center; padding: 0 50px; box-sizing: border-box;overflow-y: auto; font-size: 20px;opacity: 0;transition: opacity 0.4s;" 
         const newElChild= document.createElement('div');
-        newElChild.style.cssText = "margin: auto";
+        newElChild.style.cssText = "margin: auto;word-wrap: break-word;width: 100%;";
         newElChild.innerText = bind.value || el.innerText;
         newEl.appendChild(newElChild)
         document.body.appendChild(newEl);
